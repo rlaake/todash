650f0fb57a302d1c4cae7529ae1edc72c4bc6d30
rlaake <robblaake@gmail.com>
2020-08-07 15:37:08 -0400

    create TodosList component

--- src/App.vue
+++ src/App.vue
@@ -39,56 +39,7 @@
 
       <ProjectsList class-list="is-one-fifth is-hidden-touch"></ProjectsList>
 
-      <div class="column is-paddingless" style="background-color: whitesmoke">
-        <div class="scroll-container">
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a class="level-item">
-                <span class="is-size-3">Cooking</span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-size-5"><i class="fas fa-calendar"></i></span>
-              </a>
-            </div>
-          </nav>
-          <article class="media" v-on:mouseover="toggleButtons" v-on:mouseout="toggleButtons">
-            <figure class="media-left is-size-4" style="align-self: center; padding-top: 0; padding-right: 0.5rem">
-              <i class="far fa-dot-circle"></i>
-            </figure>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <small style="color: red;">! Important</small>
-                  <br>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-                </p>
-              </div>
-            </div>
-            <div class="buttons media-right" style="align-self: center;" v-bind:class="{'is-hidden': !showButtons}">
-              <button class="button is-info"><i class="fas fa-edit"></i></button>
-              <button class="button is-danger"><i class="fas fa-trash-alt"></i></button>
-            </div>
-          </article>
-          <article class="media" v-on:mouseover="toggleButtons" v-on:mouseout="toggleButtons">
-            <figure class="media-left is-size-4" style="align-self: center; padding-top: 0; padding-right: 0.5rem">
-              <i class="far fa-dot-circle"></i>
-            </figure>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <small style="color: red;" v-bind:class="{'is-hidden': true}">! Important</small>
-                  <br>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-                </p>
-              </div>
-            </div>
-            <div class="buttons media-right" style="align-self: center;" v-bind:class="{'is-hidden': !showButtons}">
-              <button class="button is-info"><i class="fas fa-edit"></i></button>
-              <button class="button is-danger"><i class="fas fa-trash-alt"></i></button>
-            </div>
-          </article>
-        </div>
-      </div>
+      <TodosList></TodosList>
 
     <!-- End of columns -->
     </div>
@@ -99,6 +50,7 @@
 
 <script>
 import ProjectsList from './components/ProjectsList.vue'
+import TodosList from './components/TodosList.vue'
 export default {
   name: 'App',
   data () {
@@ -109,7 +61,8 @@ export default {
     }
   },
   components: {
-    ProjectsList
+    ProjectsList,
+    TodosList
   },
   created () {
     window.addEventListener('resize', this.checkForActiveNav)
@@ -158,16 +111,5 @@ export default {
     display: flex !important;
     flex-direction: column;
     flex: 1;
-  }
-  .level-left {
-    padding-left: 3rem;
-    padding-top: 0.5rem;
-  }
-  article {
-    padding: 1rem;
-    min-height: 40px;
-  }
-  nav.level {
-    margin: 0;
   }
 </style>
---
+++ src/components/TodosList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="column is-paddingless" style="background-color: whitesmoke">
+    <div class="scroll-container">
+      <nav class="level is-mobile">
+        <div class="level-left">
+          <a class="level-item">
+            <span class="is-size-3">Cooking</span>
+          </a>
+          <a class="level-item">
+            <span class="icon is-size-5"><i class="fas fa-calendar"></i></span>
+          </a>
+        </div>
+      </nav>
+      <article class="media" v-on:mouseover="toggleButtons" v-on:mouseout="toggleButtons">
+        <figure class="media-left is-size-4" style="align-self: center; padding-top: 0; padding-right: 0.5rem">
+          <i class="far fa-dot-circle"></i>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <small style="color: red;">! Important</small>
+              <br>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
+        </div>
+        <div class="buttons media-right" style="align-self: center;" v-bind:class="{'is-hidden': !showButtons}">
+          <button class="button is-info"><i class="fas fa-edit"></i></button>
+          <button class="button is-danger"><i class="fas fa-trash-alt"></i></button>
+        </div>
+      </article>
+      <article class="media" v-on:mouseover="toggleButtons" v-on:mouseout="toggleButtons">
+        <figure class="media-left is-size-4" style="align-self: center; padding-top: 0; padding-right: 0.5rem">
+          <i class="far fa-dot-circle"></i>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <small style="color: red;" v-bind:class="{'is-hidden': true}">! Important</small>
+              <br>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
+        </div>
+        <div class="buttons media-right" style="align-self: center;" v-bind:class="{'is-hidden': !showButtons}">
+          <button class="button is-info"><i class="fas fa-edit"></i></button>
+          <button class="button is-danger"><i class="fas fa-trash-alt"></i></button>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodosList'
+}
+</script>
+
+<style scoped>
+.level-left {
+    padding-left: 3rem;
+    padding-top: 0.5rem;
+  }
+  article {
+    padding: 1rem;
+    min-height: 40px;
+  }
+  nav.level {
+    margin: 0;
+  }
+</style>
