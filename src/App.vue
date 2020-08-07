6598b3d853cdb79e89019d0b1e0bcca56862b2e4
rlaake <robblaake@gmail.com>
2020-08-07 15:43:24 -0400

    Create Navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,15 @@
 <template>
   <div id="app">
-
-    <nav class="navbar" role="navigation" aria-label="main navigation" v-bind:class="{'active-nav':showNav}">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="http://localhost:8080">
-          <img src="./assets/images/logo.svg" width="50" height="100">
-          Todash
-        </a>
-        <div class="navbar-item">{{today}}</div>
-        <a role="button" class="navbar-burger burger" v-on:click="toggleNav" v-bind:class="{'is-active':showNav}" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbarInfo" v-bind:class="{'is-active':showNav, 'active-nav':showNav}" class="navbar-menu is-paddingless">
-        <div class="navbar-start" v-bind:class="{'is-hidden': !showNav, 'active-nav':showNav}">
-          <div class="columns is-mobile is-marginless" v-bind:class="{'active-nav':showNav}">
-            <ProjectsList class-list=""></ProjectsList>
-          </div>
-        </div>
-        <div class="navbar-end" v-bind:class="{'is-hidden':showNav}">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a class="button is-light">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-
+    <Navigation></Navigation>
     <div class="columns is-mobile is-marginless" v-bind:class="{'is-hidden': showNav}">
-
       <ProjectsList class-list="is-one-fifth is-hidden-touch"></ProjectsList>
-
       <TodosList></TodosList>
-
-    <!-- End of columns -->
     </div>
-
-  <!-- End of app -->
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation.vue'
 import ProjectsList from './components/ProjectsList.vue'
 import TodosList from './components/TodosList.vue'
 export default {
@@ -61,6 +22,7 @@ export default {
     }
   },
   components: {
+    Navigation,
     ProjectsList,
     TodosList
   },
---
+++ src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation" v-bind:class="{'active-nav':showNav}">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="http://localhost:8080">
+        <img src="../assets/images/logo.svg" width="50" height="100">
+        Todash
+      </a>
+      <div class="navbar-item">{{today}}</div>
+      <a role="button" class="navbar-burger burger" v-on:click="toggleNav" v-bind:class="{'is-active':showNav}" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div id="navbarInfo" v-bind:class="{'is-active':showNav, 'active-nav':showNav}" class="navbar-menu is-paddingless">
+      <div class="navbar-start" v-bind:class="{'is-hidden': !showNav, 'active-nav':showNav}">
+        <div class="columns is-mobile is-marginless" v-bind:class="{'active-nav':showNav}">
+          <ProjectsList class-list=""></ProjectsList>
+        </div>
+      </div>
+      <div class="navbar-end" v-bind:class="{'is-hidden':showNav}">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<style scoped>
+
+</style>
